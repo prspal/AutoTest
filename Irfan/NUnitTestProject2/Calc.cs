9aec501142c041725bba09cb76365b2f380cbba6
mdirfanulla45 <mdirfanulla45@gmail.com>
2020-05-05 13:05:26 +0530

    Calculator program

---
+++ Irfan/NUnitTestProject2/Calc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace NUnitTestProject2
+{
+    class Calc
+    {
+        public int Add(int a, int b)
+        {
+            return (a + b);
+        }
+        public int Sub(int a, int b)
+        {
+            return (a - b);
+        }
+        public int Mul(int a, int b)
+        {
+            return (a * b);
+        }
+        public float Div(int a, int b)
+        {
+            return (a / b);
+        }
+    }
+}
--- Irfan/NUnitTestProject2/UnitTest1.cs
+++ Irfan/NUnitTestProject2/UnitTest1.cs
@@ -2,17 +2,30 @@ using NUnit.Framework;
 
 namespace NUnitTestProject2
 {
+    [TestFixture]
     public class Tests
     {
+        Calc c = null;
         [SetUp]
         public void Setup()
         {
+            c = new Calc();
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(6, 3, TestName = "Divide1", Description = "Test divide1")]
+        [TestCase(6, 0, TestName = "Divide2", Description = "Test divide2")]
+        public void Divide(int x,int y)
         {
-            Assert.Pass();
+           // int x = 6;
+           // int y = 3;
+            if (y == 0)
+            {
+                Assert.False(y==0,"Number cannot divide by zero");
+            }else
+            {
+                float res = c.Div(x, y);
+                Assert.AreEqual((x / y), res, "Fail");
+            }
         }
     }
 }
